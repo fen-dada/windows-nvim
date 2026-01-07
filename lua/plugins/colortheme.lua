b5d307f72b073f9e4c8531c51ab2f36b8e0fbf3d
Fendada <1823215739@qq.com>
2026-01-07 19:36:59 +0800

    hehe

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,33 +1,17 @@
 return {
-  'rebelot/kanagawa.nvim',
+  'folke/tokyonight.nvim',
+  lazy = false, -- ⭐ 关键：不要延迟加载
+  priority = 1000, -- ⭐ 关键：最先加载主题
   config = function()
-    -- Default options:
-    require('kanagawa').setup {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = 'wave', -- Load "wave" theme
-      background = { -- map the value of 'background' option to a theme
-        dark = 'wave', -- try "dragon" !
-        light = 'lotus',
+    require('tokyonight').setup {
+      style = 'moon', -- night / storm / moon / day
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
       },
     }
 
-    -- setup must be called before loading
-    vim.cmd 'colorscheme kanagawa'
+    vim.cmd 'colorscheme tokyonight'
   end,
 }
